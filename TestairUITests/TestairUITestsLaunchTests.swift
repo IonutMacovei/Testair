7a16c139d21ce6ee1f5d162d270b5d20cc54c1aa
Ionut Macovei <ionut.macovei@tazz.ro>
2022-05-15 11:47:39 +0300

    Initial Commit

---
+++ TestairUITests/TestairUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  TestairUITestsLaunchTests.swift
+//  TestairUITests
+//
+//  Created by Ionut Macovei on 15.05.2022.
+//
+
+import XCTest
+
+class TestairUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
